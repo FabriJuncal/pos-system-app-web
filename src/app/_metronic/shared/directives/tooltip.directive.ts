7ff02a95d65e01bc7c74c0e51094b56c8f62f423
FabriJuncal <juncalfabri@gmail.com>
2024-02-29 16:09:08 -0300

    refactor(tooltip component): se refactorizó el código del componente tooltip

--- src/app/_metronic/shared/directives/tooltip.directive.ts
+++ src/app/_metronic/shared/directives/tooltip.directive.ts
@@ -1,15 +1,28 @@
-import { Directive, ElementRef, AfterViewInit } from '@angular/core';
+import { Directive, ElementRef, AfterViewInit, OnDestroy } from '@angular/core';
 import { Tooltip } from 'bootstrap';
 
 @Directive({
   selector: '[appTooltip]'
 })
-export class TooltipDirective implements AfterViewInit  {
+export class TooltipDirective implements AfterViewInit, OnDestroy {
+  private tooltip: Tooltip | null = null; // Tipo Tooltip para tooltip
 
   constructor(private el: ElementRef) { }
 
   ngAfterViewInit(): void {
-    new Tooltip(this.el.nativeElement);
+    this.tooltip = new Tooltip(this.el.nativeElement);
+    this.el.nativeElement.addEventListener('click', this.hideTooltip.bind(this)); // Bind this para mantener el contexto
   }
 
+  ngOnDestroy(): void {
+    if (this.tooltip) {
+      this.tooltip.dispose(); // Limpiar el tooltip cuando se destruye la directiva
+    }
+  }
+
+  private hideTooltip(): void {
+    if (this.tooltip) {
+      this.tooltip.hide(); // Ocultar el tooltip al hacer clic en el elemento
+    }
+  }
 }
